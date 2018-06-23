e9d0955cfedc1f2589ffbbbfb91f87c47a10edb5
Jasbir Harnal <jlh2av@virginia.edu>
2018-06-23 00:23:37 -0400

    set up loop and initial account values

--- firesim.py
+++ firesim.py
@@ -4,21 +4,19 @@ import tax, params
 
 
 
+# initialize account values
+account_savings              = params.initial_value_savings
+account_retirement_401k      = params.initial_value_retirement_401k
+account_retirement_taxable   = params.initial_value_retirement_taxable
+account_roth                 = params.initial_value_roth
+account_hsa                  = params.initial_value_hsa
+
+# calculate intermediate values inside loop, publish values after each iteration
 
 
 
 
 
 
-
-
-
-
-
-
-#def main():
-
-
-
-
-
+for year in range(params.start_year, params.end_year):
+	print(year)
