f9b367317fdf0675bd37644afc6d67f070e7fea2
Jasbir Harnal <jlh2av@virginia.edu>
2018-06-25 23:16:04 -0400

    changed order of calculations, removed some unnecessary functions due to simplicity, added printing levels for debugging

--- firesim.py
+++ firesim.py
@@ -3,11 +3,7 @@
 import tax
 from params import *
 
-# account_savings = 
-# account_retirement_401k = 
-# account_retirement_taxable = 
-# account_roth = 
-# account_hsa = 
+print_level = 'accounts'
 
 # initialize account values
 
@@ -23,14 +19,6 @@ account_hsa                  = initial_value_hsa
 def inflation(initial_value, year):
 	return ((1 + inflation_rate) ** (year - start_year))* initial_value
 
-def calculateWorkIncome(age, year):
-	income = 0
-	if age < fire_age:
-		# income comes from work here
-		income = inflation(initial_value_work_income, year)
-	return income
-
-
 def calculateTaxableIncome(age, income, year):
 	if age < fire_age:
 		deductions = deduction_insurances + deduction_standard + \
@@ -40,12 +28,6 @@ def calculateTaxableIncome(age, income, year):
 	else:
 		return 0
 
-def calculate401k(current_value, age):
-	if age < fire_age:
-		return current_value + contribution_401k
-	else: 
-		return current_value
-
 def calculateTakeHome(work_income, deduction_insurances, contribution_401k, \
 	contribution_hsa, total_tax):
 	# TODO ignoring inflation
@@ -53,30 +35,54 @@ def calculateTakeHome(work_income, deduction_insurances, contribution_401k, \
 		contribution_hsa - total_tax
 
 
-
-print('Year Age Income 401k Taxable TotalTax TakeHome')
+if print_level == 'debug':
+	print('Year Age WorkIncome 401k Taxable TotalTax TakeHome')
+elif print_level == 'intermediates':
+	print('not implemented yet')
+elif print_level == 'accounts':
+	print('Year Age Roth HSA Savings 401k Taxable')
 
 for year in range(start_year, end_year):
 	# Time values
 	age = start_age + year - start_year
-	# Income
-	work_income = calculateWorkIncome(age, year)
-	# returns from investments
-	# Pre-tax contributions
-	account_retirement_401k = calculate401k(account_retirement_401k, age)
-	# hsa
-	# Taxes
-	taxable_income = calculateTaxableIncome(age, work_income, year)
-	fica_tax = tax.ficaTax(work_income, year)
-	federal_income_tax = tax.federalIncomeTax(taxable_income, tax_bracket_2018)
-	state_income_tax = tax.stateIncomeTax(work_income)
-	local_income_tax = tax.localIncomeTax(work_income)
-	total_tax = fica_tax + federal_income_tax + state_income_tax + local_income_tax
-	# Now I get paid
-	take_home = calculateTakeHome(work_income, deduction_insurances, \
-		contribution_401k, contribution_hsa, total_tax)
 
-	# 
+	# TODO calculate the things that are the same each year
+	account_retirement_401k = account_retirement_401k * (1 + growth_rate_retirement_401k)
 
-	print("%5d %5d %8.f %8.f %8.f %8.f %8.f" % 
-		(year, age, work_income, account_retirement_401k, taxable_income, total_tax, take_home))
+
+	# then calculate the things that are different depending on fire
+
+	# the work regime
+	if age < fire_age:
+		# Income from working, adjusted for inflation
+		work_income = inflation(initial_value_work_income, year)
+		# returns from investments
+		# Pre-tax contributions
+		account_retirement_401k = account_retirement_401k + contribution_401k
+		# hsa
+		# Taxes
+		taxable_income = calculateTaxableIncome(age, work_income, year)
+		fica_tax = tax.ficaTax(work_income, year)
+		federal_income_tax = tax.federalIncomeTax(taxable_income, tax_bracket_2018)
+		state_income_tax = tax.stateIncomeTax(work_income)
+		local_income_tax = tax.localIncomeTax(work_income)
+		total_tax = fica_tax + federal_income_tax + state_income_tax + local_income_tax
+		# Now I get paid
+		take_home = calculateTakeHome(work_income, deduction_insurances, \
+			contribution_401k, contribution_hsa, total_tax)
+
+	# post-work
+	else:
+		work_income = 0
+
+
+	# TODO make this less repetitive because they are subsets
+	if print_level == 'debug':
+		print("%5d %5d %8.f %8.f %8.f %8.f %8.f" % 
+			(year, age, work_income, account_retirement_401k, taxable_income, total_tax, take_home))
+	elif print_level == 'intermediates':
+		print('not implemented yet')
+	elif print_level == 'accounts':
+		print("%5d %5d %8.f %8.f %8.f %8.f %8.f" % 
+			(year, age, account_roth, account_hsa, account_savings, \
+				account_retirement_401k, account_retirement_taxable))
